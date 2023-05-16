797731bcf778ffc0f010f4fdacb3e0f0c5be9008
Mingyang Li <mingyang.li@mattr.global>
2023-05-16 16:45:44 +1200

    feat: map dynamic key type to schema

--- src/schemas/claim-source.schema.ts
+++ src/schemas/claim-source.schema.ts
@@ -1,14 +1,22 @@
 import { z } from 'zod';
 
+const authorisationSchema = z.object({
+  type: z.string(),
+  value: z.string(),
+});
+
+const requestParamSchema = z.object({
+  mapFrom: z.string().optional(),
+  required: z.boolean().optional(),
+  defaultValue: z.string().optional(),
+});
+
 export const claimSourceSchema = z.object({
   id: z.string(),
   url: z.string(),
   name: z.string(),
-  authorization: z.object({
-    type: z.string(),
-    value: z.string(),
-  }),
-  requestParameters: z.any(),
+  authorization: authorisationSchema,
+  requestParameters: z.record(requestParamSchema),
 });
 
 export type GetClaimSourcesResBody = z.infer<typeof claimSourceSchema>;
